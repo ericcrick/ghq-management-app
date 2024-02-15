94461d06b78762c3ea753f8dff570197f6709109
ericcrick <nanaoseieric6@gmail.com>
2024-02-15 11:00:17 +0000

    added custom component

--- app/components/Booking/booking.tsx
+++ app/components/Booking/booking.tsx
@@ -1,90 +1,138 @@
-'use client'
+"use client";
 
-import { Fragment } from 'react'
+import { Fragment } from "react";
 import {
   ChevronDownIcon,
   ChevronLeftIcon,
   ChevronRightIcon,
   ClockIcon,
   EllipsisHorizontalIcon,
-} from '@heroicons/react/20/solid'
-import { Menu, Transition } from '@headlessui/react'
+} from "@heroicons/react/20/solid";
+import { Menu, Transition } from "@headlessui/react";
 
 const days = [
-  { date: '2021-12-27', events: [] },
-  { date: '2021-12-28', events: [] },
-  { date: '2021-12-29', events: [] },
-  { date: '2021-12-30', events: [] },
-  { date: '2021-12-31', events: [] },
-  { date: '2022-01-01', isCurrentMonth: true, events: [] },
-  { date: '2022-01-02', isCurrentMonth: true, events: [] },
+  { date: "2021-12-27", events: [] },
+  { date: "2021-12-28", events: [] },
+  { date: "2021-12-29", events: [] },
+  { date: "2021-12-30", events: [] },
+  { date: "2021-12-31", events: [] },
+  { date: "2022-01-01", isCurrentMonth: true, events: [] },
+  { date: "2022-01-02", isCurrentMonth: true, events: [] },
   {
-    date: '2022-01-03',
+    date: "2022-01-03",
     isCurrentMonth: true,
     events: [
-      { id: 1, name: 'Design review', time: '10AM', datetime: '2022-01-03T10:00', href: '#' },
-      { id: 2, name: 'Sales meeting', time: '2PM', datetime: '2022-01-03T14:00', href: '#' },
+      {
+        id: 1,
+        name: "Design review",
+        time: "10AM",
+        datetime: "2022-01-03T10:00",
+        href: "#",
+      },
+      {
+        id: 2,
+        name: "Sales meeting",
+        time: "2PM",
+        datetime: "2022-01-03T14:00",
+        href: "#",
+      },
     ],
   },
-  { date: '2022-01-04', isCurrentMonth: true, events: [] },
-  { date: '2022-01-05', isCurrentMonth: true, events: [] },
-  { date: '2022-01-06', isCurrentMonth: true, events: [] },
+  { date: "2022-01-04", isCurrentMonth: true, events: [] },
+  { date: "2022-01-05", isCurrentMonth: true, events: [] },
+  { date: "2022-01-06", isCurrentMonth: true, events: [] },
   {
-    date: '2022-01-07',
+    date: "2022-01-07",
     isCurrentMonth: true,
-    events: [{ id: 3, name: 'Date night', time: '6PM', datetime: '2022-01-08T18:00', href: '#' }],
+    events: [
+      {
+        id: 3,
+        name: "Date night",
+        time: "6PM",
+        datetime: "2022-01-08T18:00",
+        href: "#",
+      },
+    ],
   },
-  { date: '2022-01-08', isCurrentMonth: true, events: [] },
-  { date: '2022-01-09', isCurrentMonth: true, events: [] },
-  { date: '2022-01-10', isCurrentMonth: true, events: [] },
-  { date: '2022-01-11', isCurrentMonth: true, events: [] },
+  { date: "2022-01-08", isCurrentMonth: true, events: [] },
+  { date: "2022-01-09", isCurrentMonth: true, events: [] },
+  { date: "2022-01-10", isCurrentMonth: true, events: [] },
+  { date: "2022-01-11", isCurrentMonth: true, events: [] },
   {
-    date: '2022-01-12',
+    date: "2022-01-12",
     isCurrentMonth: true,
     isToday: true,
-    events: [{ id: 6, name: "Sam's birthday party", time: '2PM', datetime: '2022-01-25T14:00', href: '#' }],
+    events: [
+      {
+        id: 6,
+        name: "Sam's birthday party",
+        time: "2PM",
+        datetime: "2022-01-25T14:00",
+        href: "#",
+      },
+    ],
   },
-  { date: '2022-01-13', isCurrentMonth: true, events: [] },
-  { date: '2022-01-14', isCurrentMonth: true, events: [] },
-  { date: '2022-01-15', isCurrentMonth: true, events: [] },
-  { date: '2022-01-16', isCurrentMonth: true, events: [] },
-  { date: '2022-01-17', isCurrentMonth: true, events: [] },
-  { date: '2022-01-18', isCurrentMonth: true, events: [] },
-  { date: '2022-01-19', isCurrentMonth: true, events: [] },
-  { date: '2022-01-20', isCurrentMonth: true, events: [] },
-  { date: '2022-01-21', isCurrentMonth: true, events: [] },
+  { date: "2022-01-13", isCurrentMonth: true, events: [] },
+  { date: "2022-01-14", isCurrentMonth: true, events: [] },
+  { date: "2022-01-15", isCurrentMonth: true, events: [] },
+  { date: "2022-01-16", isCurrentMonth: true, events: [] },
+  { date: "2022-01-17", isCurrentMonth: true, events: [] },
+  { date: "2022-01-18", isCurrentMonth: true, events: [] },
+  { date: "2022-01-19", isCurrentMonth: true, events: [] },
+  { date: "2022-01-20", isCurrentMonth: true, events: [] },
+  { date: "2022-01-21", isCurrentMonth: true, events: [] },
   {
-    date: '2022-01-22',
+    date: "2022-01-22",
     isCurrentMonth: true,
     isSelected: true,
     events: [
-      { id: 4, name: 'Maple syrup museum', time: '3PM', datetime: '2022-01-22T15:00', href: '#' },
-      { id: 5, name: 'Hockey game', time: '7PM', datetime: '2022-01-22T19:00', href: '#' },
+      {
+        id: 4,
+        name: "Maple syrup museum",
+        time: "3PM",
+        datetime: "2022-01-22T15:00",
+        href: "#",
+      },
+      {
+        id: 5,
+        name: "Hockey game",
+        time: "7PM",
+        datetime: "2022-01-22T19:00",
+        href: "#",
+      },
     ],
   },
-  { date: '2022-01-23', isCurrentMonth: true, events: [] },
-  { date: '2022-01-24', isCurrentMonth: true, events: [] },
-  { date: '2022-01-25', isCurrentMonth: true, events: [] },
-  { date: '2022-01-26', isCurrentMonth: true, events: [] },
-  { date: '2022-01-27', isCurrentMonth: true, events: [] },
-  { date: '2022-01-28', isCurrentMonth: true, events: [] },
-  { date: '2022-01-29', isCurrentMonth: true, events: [] },
-  { date: '2022-01-30', isCurrentMonth: true, events: [] },
-  { date: '2022-01-31', isCurrentMonth: true, events: [] },
-  { date: '2022-02-01', events: [] },
-  { date: '2022-02-02', events: [] },
-  { date: '2022-02-03', events: [] },
+  { date: "2022-01-23", isCurrentMonth: true, events: [] },
+  { date: "2022-01-24", isCurrentMonth: true, events: [] },
+  { date: "2022-01-25", isCurrentMonth: true, events: [] },
+  { date: "2022-01-26", isCurrentMonth: true, events: [] },
+  { date: "2022-01-27", isCurrentMonth: true, events: [] },
+  { date: "2022-01-28", isCurrentMonth: true, events: [] },
+  { date: "2022-01-29", isCurrentMonth: true, events: [] },
+  { date: "2022-01-30", isCurrentMonth: true, events: [] },
+  { date: "2022-01-31", isCurrentMonth: true, events: [] },
+  { date: "2022-02-01", events: [] },
+  { date: "2022-02-02", events: [] },
+  { date: "2022-02-03", events: [] },
   {
-    date: '2022-02-04',
-    events: [{ id: 7, name: 'Cinema with friends', time: '9PM', datetime: '2022-02-04T21:00', href: '#' }],
+    date: "2022-02-04",
+    events: [
+      {
+        id: 7,
+        name: "Cinema with friends",
+        time: "9PM",
+        datetime: "2022-02-04T21:00",
+        href: "#",
+      },
+    ],
   },
-  { date: '2022-02-05', events: [] },
-  { date: '2022-02-06', events: [] },
-]
-const selectedDay = days.find((day) => day.isSelected)
+  { date: "2022-02-05", events: [] },
+  { date: "2022-02-06", events: [] },
+];
+const selectedDay = days.find((day) => day.isSelected);
 
 function classNames(...classes: string[]) {
-  return classes.filter(Boolean).join(' ')
+  return classes.filter(Boolean).join(" ");
 }
 
 export default function Booking() {
@@ -125,7 +173,10 @@ export default function Booking() {
                 className="flex items-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
               >
                 Month view
-                <ChevronDownIcon className="-mr-1 h-5 w-5 text-gray-400" aria-hidden="true" />
+                <ChevronDownIcon
+                  className="-mr-1 h-5 w-5 text-gray-400"
+                  aria-hidden="true"
+                />
               </Menu.Button>
 
               <Transition
@@ -144,8 +195,10 @@ export default function Booking() {
                         <a
                           href="#"
                           className={classNames(
-                            active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
-                            'block px-4 py-2 text-sm'
+                            active
+                              ? "bg-gray-100 text-gray-900"
+                              : "text-gray-700",
+                            "block px-4 py-2 text-sm"
                           )}
                         >
                           Day view
@@ -157,8 +210,10 @@ export default function Booking() {
                         <a
                           href="#"
                           className={classNames(
-                            active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
-                            'block px-4 py-2 text-sm'
+                            active
+                              ? "bg-gray-100 text-gray-900"
+                              : "text-gray-700",
+                            "block px-4 py-2 text-sm"
                           )}
                         >
                           Week view
@@ -170,8 +225,10 @@ export default function Booking() {
                         <a
                           href="#"
                           className={classNames(
-                            active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
-                            'block px-4 py-2 text-sm'
+                            active
+                              ? "bg-gray-100 text-gray-900"
+                              : "text-gray-700",
+                            "block px-4 py-2 text-sm"
                           )}
                         >
                           Month view
@@ -183,8 +240,10 @@ export default function Booking() {
                         <a
                           href="#"
                           className={classNames(
-                            active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
-                            'block px-4 py-2 text-sm'
+                            active
+                              ? "bg-gray-100 text-gray-900"
+                              : "text-gray-700",
+                            "block px-4 py-2 text-sm"
                           )}
                         >
                           Year view
@@ -225,8 +284,10 @@ export default function Booking() {
                       <a
                         href="#"
                         className={classNames(
-                          active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
-                          'block px-4 py-2 text-sm'
+                          active
+                            ? "bg-gray-100 text-gray-900"
+                            : "text-gray-700",
+                          "block px-4 py-2 text-sm"
                         )}
                       >
                         Create event
@@ -240,8 +301,10 @@ export default function Booking() {
                       <a
                         href="#"
                         className={classNames(
-                          active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
-                          'block px-4 py-2 text-sm'
+                          active
+                            ? "bg-gray-100 text-gray-900"
+                            : "text-gray-700",
+                          "block px-4 py-2 text-sm"
                         )}
                       >
                         Go to today
@@ -255,8 +318,10 @@ export default function Booking() {
                       <a
                         href="#"
                         className={classNames(
-                          active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
-                          'block px-4 py-2 text-sm'
+                          active
+                            ? "bg-gray-100 text-gray-900"
+                            : "text-gray-700",
+                          "block px-4 py-2 text-sm"
                         )}
                       >
                         Day view
@@ -268,8 +333,10 @@ export default function Booking() {
                       <a
                         href="#"
                         className={classNames(
-                          active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
-                          'block px-4 py-2 text-sm'
+                          active
+                            ? "bg-gray-100 text-gray-900"
+                            : "text-gray-700",
+                          "block px-4 py-2 text-sm"
                         )}
                       >
                         Week view
@@ -281,8 +348,10 @@ export default function Booking() {
                       <a
                         href="#"
                         className={classNames(
-                          active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
-                          'block px-4 py-2 text-sm'
+                          active
+                            ? "bg-gray-100 text-gray-900"
+                            : "text-gray-700",
+                          "block px-4 py-2 text-sm"
                         )}
                       >
                         Month view
@@ -294,8 +363,10 @@ export default function Booking() {
                       <a
                         href="#"
                         className={classNames(
-                          active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
-                          'block px-4 py-2 text-sm'
+                          active
+                            ? "bg-gray-100 text-gray-900"
+                            : "text-gray-700",
+                          "block px-4 py-2 text-sm"
                         )}
                       >
                         Year view
@@ -338,19 +409,19 @@ export default function Booking() {
               <div
                 key={day.date}
                 className={classNames(
-                  day.isCurrentMonth ? 'bg-white' : 'bg-gray-50 text-gray-500',
-                  'relative px-3 py-2'
+                  day.isCurrentMonth ? "bg-white" : "bg-gray-50 text-gray-500",
+                  "relative px-3 py-2"
                 )}
               >
                 <time
                   dateTime={day.date}
                   className={
                     day.isToday
-                      ? 'flex h-6 w-6 items-center justify-center rounded-full bg-indigo-600 font-semibold text-white'
+                      ? "flex h-6 w-6 items-center justify-center rounded-full bg-indigo-600 font-semibold text-white"
                       : undefined
                   }
                 >
-                  {day.date.split('-').pop()?.replace(/^0/, '')}
+                  {day.date.split("-").pop()?.replace(/^0/, "")}
                 </time>
                 {day.events.length > 0 && (
                   <ol className="mt-2">
@@ -369,7 +440,11 @@ export default function Booking() {
                         </a>
                       </li>
                     ))}
-                    {day.events.length > 2 && <li className="text-gray-500">+ {day.events.length - 2} more</li>}
+                    {day.events.length > 2 && (
+                      <li className="text-gray-500">
+                        + {day.events.length - 2} more
+                      </li>
+                    )}
                   </ol>
                 )}
               </div>
@@ -378,34 +453,44 @@ export default function Booking() {
           <div className="isolate grid w-full grid-cols-7 grid-rows-6 gap-px lg:hidden">
             {days?.map((day) => (
               <button
-                key={day.date}
+                key={day?.date}
                 type="button"
                 className={classNames(
-                  day?.isCurrentMonth ? 'bg-white' : 'bg-gray-50',
-                  (day?.isSelected || day.isToday) && 'font-semibold',
-                  day.isSelected && 'text-white',
-                  !day.isSelected && day.isToday && 'text-indigo-600',
-                  !day.isSelected && day.isCurrentMonth && !day.isToday && 'text-gray-900',
-                  !day.isSelected && !day.isCurrentMonth && !day.isToday && 'text-gray-500',
-                  'flex h-14 flex-col px-3 py-2 hover:bg-gray-100 focus:z-10'
+                  day?.isCurrentMonth ? "bg-white" : "bg-gray-50",
+                  ((day?.isSelected as any) || day.isToday) && "font-semibold",
+                  (day.isSelected as any) && "text-white",
+                  (!day.isSelected as any) && day.isToday && "text-indigo-600",
+                  (!day.isSelected as any) &&
+                    day.isCurrentMonth &&
+                    !day.isToday &&
+                    "text-gray-900",
+                  (!day.isSelected as any) &&
+                    !day.isCurrentMonth &&
+                    !day.isToday &&
+                    "text-gray-500",
+                  "flex h-14 flex-col px-3 py-2 hover:bg-gray-100 focus:z-10"
                 )}
               >
                 <time
                   dateTime={day.date}
                   className={classNames(
-                    day?.isSelected  && 'flex h-6 w-6 items-center justify-center rounded-full',
-                    day?.isSelected && day.isToday && 'bg-indigo-600',
-                    day.isSelected && !day.isToday && 'bg-gray-900',
-                    'ml-auto'
+                    (day?.isSelected as any) &&
+                      "flex h-6 w-6 items-center justify-center rounded-full",
+                    (day?.isSelected as any) && day.isToday && "bg-indigo-600",
+                    (day.isSelected as any) && !day.isToday && "bg-gray-900",
+                    "ml-auto"
                   )}
                 >
-                  {day.date.split('-').pop()?.replace(/^0/, '')}
+                  {day.date.split("-").pop()?.replace(/^0/, "")}
                 </time>
                 <span className="sr-only">{day.events.length} events</span>
                 {day.events.length > 0 && (
                   <span className="-mx-0.5 mt-auto flex flex-wrap-reverse">
                     {day.events.map((event) => (
-                      <span key={event.id} className="mx-0.5 mb-1 h-1.5 w-1.5 rounded-full bg-gray-400" />
+                      <span
+                        key={event.id}
+                        className="mx-0.5 mb-1 h-1.5 w-1.5 rounded-full bg-gray-400"
+                      />
                     ))}
                   </span>
                 )}
@@ -414,15 +499,24 @@ export default function Booking() {
           </div>
         </div>
       </div>
-      {selectedDay.events?.length > 0 && (
+      {selectedDay.events.length > 0 && (
         <div className="px-4 py-10 sm:px-6 lg:hidden">
           <ol className="divide-y divide-gray-100 overflow-hidden rounded-lg bg-white text-sm shadow ring-1 ring-black ring-opacity-5">
             {selectedDay?.events?.map((event) => (
-              <li key={event.id} className="group flex p-4 pr-6 focus-within:bg-gray-50 hover:bg-gray-50">
+              <li
+                key={event.id}
+                className="group flex p-4 pr-6 focus-within:bg-gray-50 hover:bg-gray-50"
+              >
                 <div className="flex-auto">
                   <p className="font-semibold text-gray-900">{event.name}</p>
-                  <time dateTime={event.datetime} className="mt-2 flex items-center text-gray-700">
-                    <ClockIcon className="mr-2 h-5 w-5 text-gray-400" aria-hidden="true" />
+                  <time
+                    dateTime={event.datetime}
+                    className="mt-2 flex items-center text-gray-700"
+                  >
+                    <ClockIcon
+                      className="mr-2 h-5 w-5 text-gray-400"
+                      aria-hidden="true"
+                    />
                     {event.time}
                   </time>
                 </div>
@@ -438,5 +532,5 @@ export default function Booking() {
         </div>
       )}
     </div>
-  )
+  );
 }
