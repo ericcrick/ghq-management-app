296800af9b16fec6c9ea4f5f6c148fbdcdeed16b
ericcrick <nanaoseieric6@gmail.com>
2024-02-27 17:39:14 +0000

    fixes

--- app/components/Employees/create.tsx
+++ app/components/Employees/create.tsx
@@ -1,33 +1,77 @@
 "use clients";
-import {
-  Fieldset,
-  ScrollArea,
-  SimpleGrid,
-  TextInput,
-} from "@mantine/core";
-
+import { ScrollArea, Select, TextInput, Textarea } from "@mantine/core";
+import { DateInput } from "@mantine/dates";
 export function AddEmployee() {
   return (
     <ScrollArea h={700} className="mb-10">
-      <Fieldset
-        legend="Create New Employee"
-        className="mx-2 mt-2 text-gray-800"
-      >
-        <SimpleGrid  className="items-center" cols={{ base: 1, sm: 2, lg: 3 }}
-      spacing={{ base: 10, sm: 'sm' }}
-      verticalSpacing={{ base: 'sm', sm: 'xl' }}>
-          <TextInput label="Your name" placeholder="Your name" />
-          <TextInput label="Email" placeholder="Email" mt="md" />
-          <TextInput label="Your name" placeholder="Your name" />
-          <TextInput label="Email" placeholder="Email" mt="md" />
-          <TextInput label="Your name" placeholder="Your name" />
-          <TextInput label="Email" placeholder="Email" mt="md" />
-          <TextInput label="Your name" placeholder="Your name" />
-          <TextInput label="Email" placeholder="Email" mt="md" />
-          <TextInput label="Your name" placeholder="Your name" />
-          <TextInput label="Email" placeholder="Email" mt="md" />
-        </SimpleGrid>
-      </Fieldset>
+      <div className="mx-2 mt-2 text-gray-800">
+        <form className="grid grid-cols-1 md:grid-cols-6 gap-4">
+          <div className="md:col-span-2">
+            <TextInput label="First Name" placeholder="First name" />
+          </div>
+          <div className="md:col-span-2">
+            <TextInput label="Last Name" placeholder="Last Name" />
+          </div>
+          <div className="md:col-span-2">
+            <TextInput label="Middle Names" placeholder="Middle Names" />
+          </div>
+          <div className="md:col-span-2">
+            <TextInput label="Email" placeholder="Email" />
+          </div>
+          <div className="md:col-span-2">
+            <DateInput
+              clearable
+              valueFormat="YYYY MMM DD"
+              label="Date of Birth"
+              placeholder="Select Date of Birth"
+            />
+          </div>
+          <div className="md:col-span-2">
+          <DateInput
+              clearable
+              valueFormat="YYYY MMM DD"
+              label="Date of Enlistment"
+              placeholder="Select Englistment Date"
+            />
+          </div>
+          <div className="md:col-span-2">
+          <Select
+              label="Designation"
+              placeholder="Designation"
+              data={["Active", "Awol", "Retired", "Resigned"]}
+              searchable
+              nothingFoundMessage="Nothing found..."
+            />
+          </div>
+          <div className="md:col-span-2">
+            <Select
+              label="Department/Unit"
+              placeholder="Choose Department/Unit"
+              data={["Arakan", "Duala", "37 Military Hospital", "Critical Care"]}
+              searchable
+              nothingFoundMessage="Nothing found..."
+            />
+          </div>
+          <div className="md:col-span-1">
+            <Select
+              label="Gender"
+              placeholder="Select Gender"
+              data={["Male", "Female"]}
+              defaultValue="Male"
+              clearable
+            />
+          </div>
+          <div className="md:col-span-2">
+            <Textarea
+              label="Address"
+              placeholder="Residential Address"
+              autosize
+              minRows={2}
+              maxRows={4}
+            />
+          </div>
+        </form>
+      </div>
     </ScrollArea>
   );
 }
